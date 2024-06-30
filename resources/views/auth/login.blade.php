31ac796ff6c6ce5138eebeb7f48f2b8a69870744
melssjks <melanienieves1009@gmail.com>
2024-06-30 15:37:18 -0500

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,62 +4,67 @@
 
 @endsection
 @section('content')
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-    <div>
-        LOGO
+<div class="flex flex-col min-h-screen">
+    <!-- Contenido principal -->
+    <div class="flex-grow flex justify-center items-center ">
+        <div class="flex flex-col md:flex-row items-center bg-white shadow-lg rounded-lg w-full max-w-4xl mt-6 px-8 py-8 mx-4 overflow-hidden sm:rounded-lg">
+            <!-- Sección de imagen a la izquierda -->
+            <div class="md:w-1/2  rounded-l-lg">
+                <img src="{{ asset('images/inicio.png') }}" alt="" class="w-full">
+            </div>
+            <!-- Formulario -->
+            <div class="md:w-1/2 p-8 rounded-r-lg">
+                <div class="text-center bg-black p-6 rounded-lg">
+                    <h2 class="text-4xl font-bold text-white mb-2">CEDITEC</h2>
+                    <p class="text-white">Centro de Documentación e Información</p>
+                </div>
+                <form method="POST" action="{{ route('login') }}" class="mt-8">
+                    @csrf
+                    <!-- Dirección de correo electrónico -->
+                    <div class="mb-4 ">
+                        <label for="email" class="block text-sm font-medium text-gray-700">Correo electrónico <span class="text-gray-700">*</span></label>
+                        <input id="email" type="email" name="email" required autofocus autocomplete="username"
+                            class="w-full p-2 border border-gray-300 rounded-md mt-1 bg-stone-100" :value="old('email')" placeholder="Correo electrónico">
+                        @error('email')
+                            <span class="text-red-500 text-sm mt-2">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <!-- Contraseña -->
+                    <div class="mb-4">
+                        <label for="password" class="block text-sm font-medium text-gray-700">Contraseña <span class="text-gray-700">*</span></label>
+                        <input id="password" type="password" name="password" required autocomplete="current-password"
+                            class="w-full p-2 border border-gray-300 rounded-md mt-1 bg-stone-100" placeholder="Contraseña">
+                        @error('password')
+                            <span class="text-red-500 text-sm mt-2">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                     <!-- Olvidó su contraseña -->
+                    <div class="flex items-center justify-end mt-4">
+                        @if (Route::has('password.request'))
+                            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            href="{{ route('password.request') }}">
+                            {{ __('¿Olvidaste tu contraseña?') }}
+                            </a>
+                        @endif
+                    </div>
+                    <!-- Botón de inicio de sesión -->
+                    <div>
+                        <button type="submit" class="w-full bg-black text-white p-2 rounded-md mt-4">Iniciar sesión</button>
+                    </div>
+                    <!-- Iniciar sesión con Google -->
+                    <div class="mt-4 text-center">
+                        <button type="button" class="w-full bg-cyan-600 text-white p-2 rounded-md flex items-center justify-center">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" alt="Google" class="w-5 h-5 mr-2">
+                            Inicia sesión como Melanie Alejandra
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                    required autofocus autocomplete="username" />
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
-            </div>
-            <title>{{ config('app.name', 'Laravel') }}</title>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
-
-                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                    autocomplete="current-password" />
-
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox"
-                        class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
-                        name="remember">
-                    <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                        href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-primary-button class="ms-3">
-                    {{ __('Log in') }}
-                </x-primary-button>
-            </div>
-        </form>
-
-    </div>
-</div>
-
 @endsection
+               
+               
+
 
