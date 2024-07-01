563111dffdbebff6a0d72530d3eac1bc0a9b686b
Tomas-coder-dev <tomasidk2004@gmail.com>
2024-07-01 03:11:29 -0500

    Mel se jateo

--- resources/views/acerca/index.blade.php
+++ resources/views/acerca/index.blade.php
@@ -1,22 +1,29 @@
 @extends('layouts.navbar')
 
 @section('content')
-    <div class="container mx-auto mt-4">
-        <div class="inline-flex mb-4">
-            <a href="#" class="text-gray-800 font-bold py-2 px-4 rounded-l block hover:bg-gray-200">
-                Nosotros
-            </a>
-            <a href="#" class="text-gray-800 font-bold py-2 px-4 rounded-r block hover:bg-gray-200">
-                Servicios
-            </a>
-        </div>
-
-        <div class="sm:flex items-center">
-            <div class="sm:w-1/2 mb-4 sm:mb-0">
-                <div id="animation-carousel" class="relative w-full" data-carousel="static">
-                    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-                        <div class="hidden duration-200 ease-linear" data-carousel-item>
-                            <img src="{{ asset('images/modelo1.jpg') }}" class="absolute block w-full h-full object-cover top-0 left-0" alt="Modelo 1">
+<div class="container mx-auto mt-8 mb-8">
+    <div class="flex justify-start mb-4 ml-20">
+        <a href="#" class="mr-4 text-black font-semibold">Nosotros</a>
+        <a href="#" class="text-black font-semibold">Servicios</a>
+    </div>
+    <div class="flex flex-col sm:flex-row items-center justify-center mb-8">
+        <div class="w-full sm:w-1/2 mb-4 sm:mb-0 flex justify-center">
+            <div class="relative" style="width: 100%; max-width: 600px; height: 400px;">
+                <div>
+                    <input class="sr-only peer" type="radio" name="carousel" id="carousel-1" checked />
+                    <div class="absolute top-0 left-0 w-full h-full rounded-lg shadow-lg transition-all duration-300 opacity-0 peer-checked:opacity-100 peer-checked:z-10 z-0">
+                        <img class="rounded-lg w-full h-full object-cover" src="{{ asset('images/modelo1.png') }}" alt="Modelo 1" />
+                        <div class="absolute top-1/2 w-full flex justify-between z-20 px-4">
+                            <label for="carousel-3" class="inline-block text-red-600 cursor-pointer -translate-x-5 rounded-full shadow-md active:translate-y-0.5">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 sm:h-10 sm:w-10" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clip-rule="evenodd" />
+                                </svg>
+                            </label>
+                            <label for="carousel-2" class="inline-block text-red-600 cursor-pointer translate-x-5 rounded-full shadow-md active:translate-y-0.5">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 sm:h-10 sm:w-10" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </label>
                         </div>
                     </div>
                 </div>
--- resources/views/catalogo/index.blade.php
+++ resources/views/catalogo/index.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.navbar')
+
 @section('content')
 <div class="min-h-screen flex flex-col">
-    <div class="flex-grow container mx-auto mt-8 mb-20 flex">
-        <!-- Formulario-->
-        <div id="form-container" class="w-1/3 p-4 bg-white shadow-md rounded-lg">
+    <div class="flex-grow container mx-auto mt-8 mb-20 flex flex-col lg:flex-row">
+        <div id="form-container"
+            class="w-full lg:w-1/3 p-4 bg-white shadow-md rounded-lg mb-4 lg:mb-0 lg:mr-4 overflow-hidden">
             <h2 class="text-xl font-semibold mb-4">Catálogo</h2>
             <form>
                 <div class="mb-4">
@@ -22,11 +23,13 @@
                         <option>Campus Trujillo</option>
                     </select>
                 </div>
-                <button type="button" id="toggle-advanced" class="text-blue-500 text-sm mt-2 inline-block underline">Búsqueda avanzada</button>
+                <button type="button" id="toggle-advanced"
+                    class="text-blue-500 text-sm mt-2 inline-block underline">Búsqueda avanzada</button>
 
                 <div id="advanced-options" class="hidden mt-4">
                     <div class="mb-4">
-                        <label for="tipo_documento" class="block text-gray-700 font-bold mb-2">Tipo de documento</label>
+                        <label for="tipo_documento"
+                            class="block text-gray-700 font-bold mb-2">Tipo de documento</label>
                         <select id="tipo_documento" name="tipo_documento"
                             class="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent">
                             <option>Selecciona una opción</option>
@@ -37,7 +40,8 @@
                         </select>
                     </div>
                     <div class="mb-4">
-                        <label for="carrera" class="block text-gray-700 font-bold mb-2">Departamento de carrera</label>
+                        <label for="carrera" class="block text-gray-700 font-bold mb-2">Departamento de
+                            carrera</label>
                         <select id="carrera" name="carrera"
                             class="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent">
                             <option>Selecciona una opción</option>
@@ -68,20 +72,24 @@
                         </select>
                     </div>
                 </div>
-                <button type="submit"
-                    class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-6 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-                    Buscar
-                </button>
+                <div class="mt-4">
+                    <button type="submit"
+                        class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                        Buscar
+                    </button>
+                </div>
             </form>
         </div>
 
         <!-- Resultados -->
-        <div class="w-3/4 p-4 relative ml-6">
+        <div class="w-full lg:w-2/3 p-4">
             <h2 class="text-xl font-semibold mb-4">Resultados</h2>
-            <div id="selected-book-info" class="hidden p-4 bg-white shadow-md rounded-lg fixed top-16 right-16">
+            <div id="selected-book-info"
+                class="hidden p-4 bg-white shadow-md rounded-lg fixed top-16 right-0 lg:right-16 lg:w-80">
                 <div class="flex justify-between items-center mb-4">
                     <h3 class="text-lg font-bold">Libro seleccionado</h3>
-                    <button id="close-button" class="text-red-500 hover:text-red-700 focus:outline-none">
+                    <button id="close-button"
+                        class="text-red-500 hover:text-red-700 focus:outline-none">
                         &times;
                     </button>
                 </div>
@@ -91,54 +99,70 @@
                     Prestar libro
                 </button>
             </div>
-            <div class="grid grid-cols-2 gap-4">
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 <!-- Cards -->
-                <div class="p-4 bg-white shadow-md rounded-lg cursor-pointer select-none flex" onclick="selectBook(this, 'Simulation with arena')">
-                    <img src="URL_DE_LA_IMAGEN_DEL_LIBRO" alt="Libro" class="w-1/3 object-cover rounded-l-lg">
-                    <div class="ml-4">
+                <div class="p-4 bg-white shadow-md rounded-lg cursor-pointer select-none flex flex-col"
+                    onclick="selectBook(this, 'Simulation with arena')">
+                    <img src="{{ asset('images/simulation.jpg') }}" alt="Libro"
+                        class="w-full h-96 sm:h-auto object-cover rounded-lg mb-4">
+                    <div class="flex flex-col">
                         <h3 class="text-lg font-bold mb-2">Simulation with arena</h3>
-                        <p class="text-gray-700">Por: Katon, W. David, autor.</p>
+                        <p class="text-gray-700">Por: Katon, W. David</p>
                         <p class="text-gray-700">Fecha de publicación: 2024</p>
-                        <p class="text-gray-700">Edición: Séptima edición.</p>
-                        <p class="text-gray-700">Clasificación: 0033 K37</p>
-                        <p class="text-gray-700">Formato: Libros</p>
-                        <p class="text-gray-700">Disponible: 1</p>
+                        <div class="mt-auto">
+                            <p class="text-gray-700">Edición: Séptima edición</p>
+                            <p class="text-gray-700">Clasificación: 0033 K37</p>
+                            <p class="text-gray-700">Formato: Libros</p>
+                            <p class="text-gray-700">Disponible: 1</p>
+                        </div>
                     </div>
                 </div>
-                <div class="p-4 bg-white shadow-md rounded-lg cursor-pointer select-none flex" onclick="selectBook(this, 'Otro libro')">
-                    <img src="URL_DE_LA_IMAGEN_DEL_LIBRO" alt="Libro" class="w-1/3 object-cover rounded-l-lg">
-                    <div class="ml-4">
-                        <h3 class="text-lg font-bold mb-2">Otro libro</h3>
-                        <p class="text-gray-700">Por: Autor Ejemplo.</p>
+                <div class="p-4 bg-white shadow-md rounded-lg cursor-pointer select-none flex flex-col"
+                    onclick="selectBook(this, 'Introduccion a la Programacion')">
+                    <img src="{{ asset('images/introduccion.jpg') }}" alt="Libro"
+                        class="w-full h-96 sm:h-auto object-cover rounded-lg mb-4">
+                    <div class="flex flex-col">
+                        <h3 class="text-lg font-bold mb-2">Introduccion a la Programacion</h3>
+                        <p class="text-gray-700">Por: Charte Ojeda, Francisco</p>
                         <p class="text-gray-700">Fecha de publicación: 2024</p>
-                        <p class="text-gray-700">Edición: Primera edición.</p>
-                        <p class="text-gray-700">Clasificación: 0044 E56</p>
-                        <p class="text-gray-700">Formato: Libros</p>
-                        <p class="text-gray-700">Disponible: 3</p>
+                        <div class="mt-auto">
+                            <p class="text-gray-700">Edición: Primera edición</p>
+                            <p class="text-gray-700">Clasificación: 0044 E56</p>
+                            <p class="text-gray-700">Formato: Libros</p>
+                            <p class="text-gray-700">Disponible: 3</p>
+                        </div>
                     </div>
                 </div>
-                <div class="p-4 bg-white shadow-md rounded-lg cursor-pointer select-none flex" onclick="selectBook(this, 'Otro libro')">
-                    <img src="URL_DE_LA_IMAGEN_DEL_LIBRO" alt="Libro" class="w-1/3 object-cover rounded-l-lg">
-                    <div class="ml-4">
-                        <h3 class="text-lg font-bold mb-2">Otro libro</h3>
-                        <p class="text-gray-700">Por: Autor Ejemplo.</p>
+                <div class="p-4 bg-white shadow-md rounded-lg cursor-pointer select-none flex flex-col"
+                    onclick="selectBook(this, 'Como programar en java')">
+                    <img src="{{ asset('images/java.jpg') }}" alt="Libro"
+                        class="w-full h-96 sm:h-auto object-cover rounded-lg mb-4">
+                    <div class="flex flex-col">
+                        <h3 class="text-lg font-bold mb-2">Como programar en java</h3>
+                        <p class="text-gray-700">Por: Harvey M. Deitel</p>
                         <p class="text-gray-700">Fecha de publicación: 2024</p>
-                        <p class="text-gray-700">Edición: Primera edición.</p>
-                        <p class="text-gray-700">Clasificación: 0044 E56</p>
-                        <p class="text-gray-700">Formato: Libros</p>
-                        <p class="text-gray-700">Disponible: 3</p>
+                        <div class="mt-auto">
+                            <p class="text-gray-700">Edición: Novena edición</p>
+                            <p class="text-gray-700">Clasificación: 0044 H37</p>
+                            <p class="text-gray-700">Formato: Libros</p>
+                            <p class="text-gray-700">Disponible: 2</p>
+                        </div>
                     </div>
                 </div>
-                <div class="p-4 bg-white shadow-md rounded-lg cursor-pointer select-none flex" onclick="selectBook(this, 'Otro libro')">
-                    <img src="URL_DE_LA_IMAGEN_DEL_LIBRO" alt="Libro" class="w-1/3 object-cover rounded-l-lg">
-                    <div class="ml-4">
-                        <h3 class="text-lg font-bold mb-2">Otro libro</h3>
-                        <p class="text-gray-700">Por: Autor Ejemplo.</p>
+                <div class="p-4 bg-white shadow-md rounded-lg cursor-pointer select-none flex flex-col"
+                    onclick="selectBook(this, 'Programación Para Principiantes')">
+                    <img src="{{ asset('images/p_python.jpg') }}" alt="Libro"
+                        class="w-full h-96 sm:h-auto object-cover rounded-lg mb-4">
+                    <div class="flex flex-col">
+                        <h3 class="text-lg font-bold mb-2">Programación Para Principiantes</h3>
+                        <p class="text-gray-700">Por: Alexander Cane</p>
                         <p class="text-gray-700">Fecha de publicación: 2024</p>
-                        <p class="text-gray-700">Edición: Primera edición.</p>
-                        <p class="text-gray-700">Clasificación: 0044 E56</p>
-                        <p class="text-gray-700">Formato: Libros</p>
-                        <p class="text-gray-700">Disponible: 3</p>
+                        <div class="mt-auto">
+                            <p class="text-gray-700">Edición: Primera edición</p>
+                            <p class="text-gray-700">Clasificación: 0044 E56</p>
+                            <p class="text-gray-700">Formato: Libros</p>
+                            <p class="text-gray-700">Disponible: 3</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -155,16 +179,13 @@
     });
 
     function selectBook(element, title) {
-        // Desmarcar cualquier tarjeta previamente seleccionada
         var previouslySelected = document.querySelector('.selected');
         if (previouslySelected) {
             previouslySelected.classList.remove('selected');
         }
 
-        // Marcar la tarjeta actual como seleccionada
         element.classList.add('selected');
 
-        // Mostrar información del libro seleccionado
         var bookInfo = document.getElementById('selected-book-info');
         var bookTitle = document.getElementById('book-title');
         bookTitle.textContent = title;
